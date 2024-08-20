2333b8bdeea9c348d43c43cd05a81b524b448c60
lucassperanzini <labsfiap@fiap.com.br>
2024-08-20 12:00:08 -0300

    adicionei o céu

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import * as THREE from "three";
+import { MathUtils, Vector3 } from "three";
+import { Sky } from 'three/addons/objects/Sky.js';
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-
 // tela
 const w = window.innerWidth
 const h = window.innerHeight
 
 // render
-const renderer = new THREE.WebGLRenderer({antialias : true});
-renderer.setSize(w,h);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
 
 
 
@@ -17,7 +18,7 @@ document.body.appendChild(renderer.domElement)
 const fieldOfView = 75;
 
 // aspect ?
-const aspect = w/ h;
+const aspect = w / h;
 
 // distancia maxima que o site vai renderizar objetos
 const near = 0.1;
@@ -25,13 +26,24 @@ const near = 0.1;
 //quao longe vai renderizar
 const far = 10;
 
-const camera = new THREE.PerspectiveCamera(fieldOfView,aspect,near,far);
-camera.position.z =3;
+const camera = new THREE.PerspectiveCamera(fieldOfView, aspect, near, far);
+camera.position.z = 3;
 
 
-const scene =new THREE.Scene();
+const scene = new THREE.Scene();
 
 // controle de rotação
+
+const sky = new Sky();
+sky.scale.setScalar(450000);
+
+const phi = MathUtils.degToRad(90);
+const theta = MathUtils.degToRad(180);
+const sunPosition = new Vector3().setFromSphericalCoords(1, phi, theta);
+
+sky.material.uniforms.sunPosition.value = sunPosition;
+
+scene.add(sky);
 
 
 
@@ -40,18 +52,18 @@ const controls = new OrbitControls(camera, renderer.domElement)
 //nao sei ainda
 controls.enableDamping = true
 //o quao fluido é a rotação quanto maior o numero menos fluido
-controls.dampingFactor =0.03
+controls.dampingFactor = 0.03
 
 
 // geometria , tipo de mesh exmeplo, cubo esphera etc..
 // tipo de material se pe metalico por exemplo
-const geometry = new THREE.TorusKnotGeometry(4,0.1,100,10); // Create a cube geometry
-const material =  new THREE.MeshStandardMaterial({
+const geometry = new THREE.IcosahedronGeometry(1, 5); // Create a cube geometry
+const material = new THREE.MeshStandardMaterial({
 
     color: "white",
 
     // mostra vertices e edges do objeto
-    flatShading:true
+    flatShading: true
 })
 
 
@@ -59,30 +71,34 @@ const material =  new THREE.MeshStandardMaterial({
 
 // a mesh e o tipo de eobjeto mais seu material 
 
-const mesh = new THREE.Mesh(geometry,material);
+const mesh = new THREE.Mesh(geometry, material);
 
 const wireMat = new THREE.MeshBasicMaterial({
-    color : "white",
+    color: "white",
     wireframe: true,
 })
 
-const wireMesh = new THREE.Mesh(geometry,wireMat);
+const wireMesh = new THREE.Mesh(geometry, wireMat);
 wireMesh.scale.setScalar(1.001)
 
 mesh.add(wireMesh)
 
 
 
+// const directionalLight = new THREE.DirectionalLight("#FF00F7", 2);
+// scene.add(directionalLight);
+
+
 // tipo de iluminacao
-const hemiLight = new THREE.HemisphereLight("red" ,"blue",1.5)
+const hemiLight = new THREE.HemisphereLight("red", "blue", 1.5)
 
 scene.add(mesh);
 scene.add(hemiLight)
 
 
 // funcao para animar a cena
-function animate(){
-    renderer.render(scene,camera);
+function animate() {
+    renderer.render(scene, camera);
     requestAnimationFrame(animate);
 
     //udpate por frame do control
